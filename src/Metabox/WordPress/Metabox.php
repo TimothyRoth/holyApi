a3bd24b4990c5cbbd7d53d17818dfe28cc83457b
Timothy Roth <roth@marketport.de>
2024-03-07 17:21:35 +0100

    - updated Metabox.php

--- src/Metabox/WordPress/Metabox.php
+++ src/Metabox/WordPress/Metabox.php
@@ -8,13 +8,15 @@ class Metabox implements MetaboxInterface
 {
     public function create(string $name, callable $callback, string|array $screen = null, int|null $id = null, string $context = 'advanced', string $priority = 'default', array $callback_args = null): void
     {
-        if (is_int($id)) {
-            global $post;
-            if ($post->ID === $id) {
-                add_action('add_meta_boxes', function () use ($name, $callback, $screen, $context, $priority, $callback_args) {
+
+        if (!is_null($id)) {
+            add_action('add_meta_boxes', function () use ($name, $callback, $screen, $id, $context, $priority, $callback_args) {
+                global $post;
+                if ($post->ID === $id) {
                     add_meta_box($name, $name, $callback, $screen, $context, $priority, $callback_args);
-                });
-            }
+                }
+            });
+
             return;
         }
 
